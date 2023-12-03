1bc39cedeb964232aa0ce205c180991872ad01a4
LemonDestiny <97205245+LemonDestiny@users.noreply.github.com>
2023-12-02 21:27:23 -0300

    añadí más cotizaciones y le agregué estilo

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-let precio = document.getElementById("precio");
-
+let oficial = document.getElementById("precioOficial");
+let blue = document.getElementById("precioBlue");
+let tarjeta = document.getElementById("precioTarjeta");
+let cripto = document.getElementById("precioCripto");
 function obtenerValor() {
   fetch("https://dolarapi.com/v1/dolares/tarjeta").then((response) =>
     response.json().then((response) => (precio.innerText = response.venta))
   );
 }
 
-async function obtenerValorOtraForma() {
-  let valor = await fetch("https://dolarapi.com/v1/dolares/tarjeta");
+async function obtenerValorOtraForma(objeto, url) {
+  let valor = await fetch(url);
   let cotizacion = await valor.json();
-  precio.innerText = cotizacion.venta;
+  objeto.innerText = cotizacion.venta;
 }
-obtenerValorOtraForma();
+
+obtenerValorOtraForma(oficial, "https://dolarapi.com/v1/dolares/oficial");
+obtenerValorOtraForma(blue, "https://dolarapi.com/v1/dolares/blue");
+obtenerValorOtraForma(tarjeta, "https://dolarapi.com/v1/dolares/tarjeta");
+obtenerValorOtraForma(cripto, "https://dolarapi.com/v1/dolares/cripto");
